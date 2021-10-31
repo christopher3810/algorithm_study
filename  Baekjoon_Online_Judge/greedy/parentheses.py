d2429cdcfe25196d80567fe5a1389eac19597111
sungminyang <lonesome661@naver.com>
2021-10-31 23:46:17 +0900

    백준 그리디 1541번 잃어버린 괄호 문제 정답 코드 작성

---  Baekjoon_Online_Judge/greedy/parentheses.py
+++  Baekjoon_Online_Judge/greedy/parentheses.py
@@ -28,3 +28,49 @@
 2. spit -
 3. 연산
 '''
+'''
+s = input().split('-')
+result = 0
+if len(s) > 1:
+    for i, v in enumerate(s):
+        if i == 0:
+            if "+" in v:
+                temp = v.split("+")
+                for i in temp:
+                    i.lstrip("0")
+                    result += int(i)
+            else:
+                result += int(s[0].lstrip("0"))
+        else:
+            if "+" in v:
+                temp = v.split("+")
+                for i in temp:
+                    i.lstrip("0")
+                    result -= int(i)
+            else:
+                result -= int(v.lstrip("0"))
+    print(result)
+if len(s) == 1:
+    if "+" in s[0] :
+        temp = s[0].split("+")
+        for i in temp:
+            i.lstrip("0")
+            result += int(i)
+        print(result)
+    else:
+        print(int(s[0]))
+'''
+# 초키 로직코드 작성
+# lstrip을 사용할 필요가 없음
+# sum()사용시 리스트 합산에서 009의 형태도 계산해서 보여줌
+# 1. 맨처음 숫자를 덧셈 처리하고
+# 2. 이후 숫자들을 +를 쪼개서 리스트로 만든뒤 리스트 전체 합산값으로 만들어준뒤 뺄셈
+# 개선코드
+
+a = input().split('-')
+# 맨 앞자리 숫자는 덧셈처리 해야하니 전체 리스트에서 맨앞 pop으로 뺀뒤 스플릿해서 전체 합(- 스플릿한 첫번쨰 수는 한가지일수도 연산식일수도 있다.)
+s = sum(list(map(int, a.pop(0).split('+'))))
+for x in a :
+    s -= sum(list(map(int, x.split('+'))))
+    # 나머지 숫자 + 기준으로 스플릿후 리스트로 만든뒤 리스트 전체 한산값 - 붙여줌
+print(s)
